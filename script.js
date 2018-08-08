9dc67afe07c2d49e2d5337eb45103d4c2af3b538
Chidie Nicole Nwakalor <nicole.nwakalor@gmail.com>
2018-08-08 00:04:48 -0400

    first commit

---
+++ script.js
@@ -0,0 +1,23 @@
+function idCard(){
+  let firstName = document.getElementById("firstName").value;
+  let lastName = document.getElementById("lastName").value;
+  let address = document.getElementById("address").value;
+  document.getElementById("postFullName").innerHTML = firstName + " " + lastName;
+  document.getElementById("postAddress").innerHTML = address;
+  
+  let age = document.getElementById("age").value;
+  let phoneNumber = document.getElementById("phoneNumber").value;
+  
+  let numberArray = [];
+  numberArray.push(age, phoneNumber);
+  
+  for (let i = 0; i < numberArray.length; i++){
+    if(numberArray[i] <= 100){
+      document.getElementById("postAge").innerHTML = "Age: " + age;
+    }
+    else if(numberArray[i] > 100){
+      document.getElementById("postPhoneNumber").innerHTML = "Phone Number: " + phoneNumber;
+    }
+  }
+  
+}
